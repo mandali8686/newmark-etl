6915af465062656eae1b94e4445251fba9a77f63
manda <100242191+mandali8686@users.noreply.github.com>
2025-11-14 20:57:21 -0500

    Update settings.py

--- backend/newmark_backend/settings.py
+++ backend/newmark_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r9l0g3px@k8%aig2hcr33qw^tg9-$tv3dt-%k_nm!b*inkx0=3"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
